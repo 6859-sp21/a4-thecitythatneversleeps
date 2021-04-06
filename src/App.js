3f31fd8de3f8c6649ae6eb1cd80a670f56d03ad9
Magdalena Price <maprice@mit.edu>
2021-04-06 19:55:25 -0400

    Started building icon layer on map with filler json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
+import {HexagonLayer, GridLayer, IconLayer, ScatterplotLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
 
 import logo from './logo.svg';
@@ -14,7 +14,14 @@ const config = {
   //process.env.MapboxAccessToken
 }
 
+// determines size of circles
+// TODO: choose our own sizing
+const ICON_MAPPING = {
+  marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
+};
+
 // Viewport settings
+// We start in NYC, Manhattan 
 const INITIAL_VIEW_STATE = {
   longitude: -74.0060,
   latitude: 40.7128,
@@ -23,15 +30,43 @@ const INITIAL_VIEW_STATE = {
   bearing: 0
 };
 
-// Data to be used by the LineLayer
-const data = [
-  {sourcePosition: [-74.0060, 40.7128], targetPosition: [-74.0060, 40.7128]}
-];
+// Data to be used by the LineLayer or other Layers
+// TODO: Fill in data from csv->json
+const json_data = {
+  0: {
+      coordinates:[73.9857, 40.7484]
+    },
+  1: {
+      coordinates: [73.9665, 40.7812]
+  }
+};
+
+// Filling in our data for the IconLayer
+const iconData = []
+
+for (const obj in json_data) {
+  const loc = obj.coordinates;
+  iconData.push({sourcePosition: loc, targetPosition: loc})
+}
 
 // DeckGL react component
 function App() {
   const layers = [
-    new LineLayer({id: 'line-layer', data})
+    new IconLayer({
+      id: 'icon-layer',
+      iconData,
+      pickable: true,
+      // iconAtlas and iconMapping are required
+      // getIcon: return a string
+      // TODO: choose our own icon
+      iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+      iconMapping: ICON_MAPPING,
+      getIcon: d => 'marker',
+      sizeScale: 15,
+      getPosition: d => d.coordinates,
+      getSize: d => 5,
+      getColor: d => [Math.sqrt(d.exits), 140, 0] 
+    })
   ];
   
   return (
