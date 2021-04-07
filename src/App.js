02d3c2fcb46e05340907f8804efc7c0cc706c2f4
Melody Phu <melody.phu.8@gmail.com>
2021-04-07 02:54:48 -0400

    set up basic filtering

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ class App extends React.Component {
     super(props);
     this.state = {
       mapData: {},
+      filteredData: {},
       layers: [],
       iconData: [],
       startDatetime: 'January 1, 2019, 00:00:00 AM',
       endDatetime: 'March 31, 2021, 11:59:59 PM',
       // add any other default filter parameters here
       openSidebar: false,
+      fieldNames: ['Location Type', 'borough', 'neighborhood', 'Incident Zip'],
+      filterOptions: {}
     }
   }
 
@@ -36,39 +39,91 @@ class App extends React.Component {
 
   // gets map data
   getMapData = () => {
-    const mapData = {
+    var mapData = {
       ...data1.mapData1,
       ...data2.mapData2,
       ...data3.mapData3,
     };
+
     return mapData;
   }
 
-  // for filtering data
-  updateData = (mapData) => {
+  // TODO find all unique values of things to filter by
+  getFilterOptions(mapData) {
+    var filterOptions = {}
+    for (const key in mapData) {
+      let datapoint = mapData[key];
+
+      for (const field in this.state.fieldNames) {
+        let existingOptions = (filterOptions[field]) ? this.state.filterOptions[field] : [];
+        let value = datapoint[field];
+
+        if (!existingOptions.includes(value)) {
+          existingOptions.push(value);
+        }
+      }
+    }
+
+    return filterOptions;
+  }
+
+  // for filtering data, called by FilterSidebar.js
+  // filters = {'fieldName': [all selected options]}
+  updateMapData = (filters) => {
+    // TODO clip the dataset by the start and end datetimes
+    // for every data point
+    
+    let mapData = this.state.mapData;
+    var filteredData = {};
+
+    for (const key in mapData) {
+      let datapoint = mapData[key];
+      let shouldInclude = true;
+
+      // check all filters
+      for (const field in filters) {
+        let selectedOptions = filters[field];
+        let value = datapoint[field];
+
+        // once it doesn't match one of the filters, drop it
+        if (!selectedOptions.includes(value)) {
+          shouldInclude = false;
+        }
+      }
+      if (shouldInclude) {
+        filteredData[key] = JSON.parse(JSON.stringify(datapoint));
+      }
+    }
+
     this.setState({
-      mapData: mapData
+      filteredData: filteredData
     })
   }
   
   // when the component is first created
   componentDidMount() {
-    let mapData = this.getMapData();
-    this.updateData(mapData);
+    var mapData = this.getMapData();
+    var filterOptions = this.getFilterOptions(mapData);
+
+    this.setState({
+      mapData: mapData,
+      filterOptions: filterOptions,
+      filteredData: JSON.parse(JSON.stringify(mapData))
+    })
   }
 
   render(viewState) {
     return (
       <div>
         <BaseMap 
-          mapData={this.state.mapData}
+          mapData={this.state.filteredData}
           viewState={viewState}
         >
         </BaseMap>
         <FilterSidebar 
           updateMapData={this.updateMapData} 
-          mapData={this.state.mapData} 
           open={this.state.openSidebar}
+          filterOptions={this.state.filterOptions}
         >
         </FilterSidebar>
       </div>
